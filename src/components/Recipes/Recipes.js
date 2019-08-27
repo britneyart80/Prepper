12d43754dc60cdbfa214359bde41af63dd4a899e
britneyart80 <chen.bri@husky.neu.edu>
2019-08-27 15:53:13 -0400

    Moved create recipe button from nav Dropdown
    - create recipe button appears when you are viewing all Recipes
    instead of in the Navigation bar

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -3,6 +3,7 @@ import { withRouter } from 'react-router-dom'
 import axios from 'axios'
 import apiUrl from './../../apiConfig'
 import Spinner from 'react-bootstrap/Spinner'
+import Button from 'react-bootstrap/Button'
 import ListGroup from 'react-bootstrap/ListGroup'
 
 class Recipes extends Component {
@@ -26,11 +27,22 @@ class Recipes extends Component {
   render () {
     const recipes = this.state.recipes
     if (recipes.length) {
-      return recipes.map(recipe => (
+      const buttonJsx = (
+        <React.Fragment>
+          <Button href='#create-recipe' user={this.props.user}>Create a Recipe</Button>
+        </React.Fragment>
+      )
+      const listRecipes = recipes.map(recipe => (
         <ListGroup.Item key={recipe._id} as="a" action variant="light" href={`#recipes/${recipe._id}`}>
           {recipe.name}
         </ListGroup.Item>
       ))
+      return (
+        <React.Fragment>
+          { buttonJsx }
+          { listRecipes }
+        </React.Fragment>
+      )
     }
     return (
       <Spinner animation="border" />
